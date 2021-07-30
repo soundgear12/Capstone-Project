d4ff325577ccd20549acaf5610a775b4ef893c5b
Antonio Rodriguez <antonio.rodriguez024@gmail.com>
2021-07-30 18:28:41 -0500

    First commit

---
+++ src/main/java/com/meritamerica/capstoneproject/models/AccountHolder.java
@@ -0,0 +1,55 @@
+package com.meritamerica.capstoneproject.models;
+
+public class AccountHolder {
+
+	AccountHolder() {
+
+	}
+
+	private int id;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String ssn;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+}
